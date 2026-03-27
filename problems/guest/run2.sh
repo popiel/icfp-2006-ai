d404102bf4f667aa26d6b83e83068e07fb565d3d
Alex Popiel <tapopiel@gmail.com>
2026-03-26 17:09:35 -0700

    Checkpoint with initial access to howie and ohmega

---
+++ problems/guest/run2.sh
@@ -0,0 +1,16 @@
+(cat <<EOT; cat hack.bas; cat<<EOT) | (cd ../..; ./run.sh umix.um) 2>&1 | tee run2.output
+guest
+/bin/umodem hack.bas STOP
+EOT
+STOP
+/bin/qbasic hack.bas
+ls /home
+./hack.exe howie
+./hack.exe knr
+./hack.exe gardener
+./hack.exe ohmega
+./hack.exe yang
+./hack.exe hmonk
+./hack.exe bbarker
+exit
+EOT
