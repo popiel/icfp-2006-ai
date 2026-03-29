f422a81d0ab16f411d75602f05a2eeec1757f913
Alex Popiel <tapopiel@gmail.com>
2026-03-28 22:02:08 -0700

    Updating run scripts

--- runc.sh
+++ runc.sh
@@ -1,3 +1,3 @@
 CLASSPATH=$(sbt --batch 'export runtime:fullClasspath' 2>/dev/null | tail -1 | sed 's/\\/\//g')
-java -classpath "$CLASSPATH" com.wolfskeep.UniversalMachine "$@"
+java -classpath "$CLASSPATH" com.wolfskeep.CompiledUniversalMachine "$@"
 
